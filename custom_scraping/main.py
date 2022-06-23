c9c758fc18f13842b6337ce4358a2a6c17d220b2
gian condori <francoguns16@gmail.com>
2022-06-23 00:04:47 -0300

    primer ejemplo de internet

--- custom_scraping/main.py
+++ custom_scraping/main.py
@@ -2,16 +2,17 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-class MyScraping(CrawlSpider):
+class MyScraping(scrapy.Spider):
     name = 'myscraping'
     allowed_domains = ['aosfatos.org']
     start_urls = ['https://aosfatos.org/']
-
+    '''
     rules = (
         Rule(
             LinkExtractor(
                 restrict_xpaths='//li[contains(text(), "Checamos")]//ul/li'
-            )
+            ),
+            callback='parse_new'
         ),
         Rule(
             LinkExtractor(
@@ -20,7 +21,44 @@ class MyScraping(CrawlSpider):
             callback='parse_new'
         )
     )
+    '''
+    def parse(self, response):
+        links = response.xpath('//nav//ul//li/a[re:test(@href, "checamos")]/@href').getall()
+        for link in links:
+            yield scrapy.Request(
+                response.urljoin(link),
+                callback=self.parse_category
+            )
 
-    def paser_new(self, response):
-        import ipdb; ipdb.set_trace()
-        #title = response.css('article h1::text')
+    def parse_category(self, response):
+        news = response.css('.entry-item-card::attr(href)').getall()
+        for new_url in news:
+            yield scrapy.Request(
+                response.urljoin(new_url),
+                callback=self.parse_new
+            )
+        pages_url = response.css('.pagination a::attr(href)').getall()
+        for page in pages_url:
+            yield scrapy.Request(
+                response.urljoin(page),
+                callback=self.parse_category
+            )
+
+    def parse_new(self, response):
+        #import ipdb; ipdb.set_trace()
+        title = response.css('article h1::text').get()
+        date = ' '.join(response.css('div.publish-date::text').get().split())
+
+        quotes = response.css('article blockquote p')
+        for quote in quotes:
+            quote_text = quote.css('::text').get()
+            status = quote.xpath(
+                './parent::blockquote/preceding-sibling::figure//figcaption/text()'
+            ).get()
+            yield {
+                'title': title,
+                'date': date,
+                'quote_text': quote_text,
+                'status': status,
+                'url': response.url
+            }
